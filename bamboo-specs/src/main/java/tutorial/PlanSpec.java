982eaec492509c8392aa581c2c6a304a2695e25e
younu8084 <55754022+younu8084@users.noreply.github.com>
2020-02-11 12:50:48 +0530

    Update PlanSpec.java

--- bamboo-specs/src/main/java/tutorial/PlanSpec.java
+++ bamboo-specs/src/main/java/tutorial/PlanSpec.java
@@ -37,120 +37,30 @@ public class PlanSpec {
     
     public Plan plan() {
         final Plan plan = new Plan(new Project()
-                /*.oid(new BambooOid("cxx7l5poipz5"))*/
-                .key(new BambooKey("MYD"))
-                .name("MyDev")
-                .description("Dev project for testing CICD"),
-            "MyPlan1",
-            new BambooKey("MYZ"))
-            /*.oid(new BambooOid("cxnidkcgoxkx"))*/
-            .description("Plan for implementing CICD 1")
+                .oid(new BambooOid("sm2vi4phkhs4"))
+                .key(new BambooKey("REV"))
+                .name("revathi"),
+            "onely",
+            new BambooKey("ONE"))
             .pluginConfigurations(new ConcurrentBuilds()
                     .useSystemWideDefault(false))
-            .stages(new Stage("Continuous Integration")
-                    .jobs(new Job("CI Job",
-                            new BambooKey("JOB1"))
-                            .artifacts(new Artifact()
-                                    .name("MyWARFile")
-                                    .copyPattern("*.war")
-                                    .location("target")
-                                    .shared(true),
-                                new Artifact()
-                                    .name("Cobertura Report")
-                                    .copyPattern("*")
-                                    .location("target/site/cobertura"))
-                            .tasks(new CleanWorkingDirectoryTask()
-                                    .description("Clean working directory"),
-                                new VcsCheckoutTask()
-                                    .description("Checkout GitHub Repo")
-                                    .checkoutItems(new CheckoutItem()
-                                            .repository(new VcsRepositoryIdentifier()
-                                                    .name("sherlock")))
-                                    .cleanCheckout(true),
-                                new MavenTask()
-                                    .description("Maven with Cobertura CC report")
-                                    .goal("cobertura:cobertura -Dcobertura.report.format=xml")
-                                    .jdk("JDK 1.8")
-                                    .executableLabel("Maven 3"),
-                                new AnyTask(new AtlassianModule("ch.mibex.bamboo.sonar4bamboo:sonar4bamboo.maven3task"))
-                                    .description("Sonar Report Publisher")
-                                    .enabled(false)
-                                    .configuration(new MapBuilder()
-                                            .put("incrementalFileForInclusionList", "")
-                                            .put("chosenSonarConfigId", "1")
-                                            .put("useGradleWrapper", "")
-                                            .put("useNewGradleSonarQubePlugin", "")
-                                            .put("sonarJavaSource", "")
-                                            .put("sonarProjectName", "")
-                                            .put("buildJdk", "JDK 1.8")
-                                            .put("gradleWrapperLocation", "")
-                                            .put("sonarLanguage", "")
-                                            .put("sonarSources", "")
-                                            .put("useGlobalSonarServerConfig", "true")
-                                            .put("incrementalMode", "")
-                                            .put("failBuildForBrokenQualityGates", "")
-                                            .put("sonarTests", "")
-                                            .put("incrementalNoPullRequest", "incrementalModeFailBuildField")
-                                            .put("failBuildForSonarErrors", "")
-                                            .put("sonarProjectVersion", "")
-                                            .put("sonarBranch", "")
-                                            .put("executable", "Maven 3")
-                                            .put("illegalBranchCharsReplacement", "_")
-                                            .put("failBuildForTaskErrors", "true")
-                                            .put("incrementalModeNotPossible", "incrementalModeRunFullAnalysis")
-                                            .put("sonarJavaTarget", "")
-                                            .put("environmentVariables", "")
-                                            .put("incrementalModeGitBranchPattern", "")
-                                            .put("legacyBranching", "true")
-                                            .put("replaceSpecialBranchChars", "")
-                                            .put("additionalProperties", "")
-                                            .put("autoBranch", "true")
-                                            .put("sonarProjectKey", "")
-                                            .put("incrementalModeBambooUser", "")
-                                            .put("overrideSonarBuildConfig", "")
-                                            .put("workingSubDirectory", "")
-                                            .build()),
-                                new MavenTask()
-                                    .description("Build task")
-                                    .enabled(false)
-                                    .goal("clean compile package")
-                                    .jdk("JDK 1.8")
-                                    .executableLabel("Maven 3"))))
-            .linkedRepositories("first-project")
-            .planRepositories(new GitHubRepository()
-                    .name("sherlock")
-                   /* .oid(new BambooOid("cxro1e2p6vwh"))*/
-                    .repositoryViewer(new GitHubRepositoryViewer())
-                    .repository("Gpkmr/sherlock")
-                    .branch("master")
-                    .authentication(new UserPasswordAuthentication("gpkmr")
-                            .password("Gopi@16!"))
-                    .changeDetection(new VcsChangeDetection()))
-            
-            .triggers(new RepositoryPollingTrigger()
-                    .enabled(false)
-                    .withPollingPeriod(Duration.ofSeconds(30)),
-                new RemoteTrigger()
-                    .description("GitHub trigger")
-                    .enabled(false))
-            .planBranchManagement(new PlanBranchManagement()
-                    .delete(new BranchCleanup())
-                    .notificationForCommitters());
+            .stages(new Stage("TestStage")
+                    .jobs(new Job("New",
+                            new BambooKey("NEW")))
+                          
         return plan;
     }
     
     public PlanPermissions planPermission() {
-        final PlanPermissions planPermission = new PlanPermissions(new PlanIdentifier("MYD", "MYZ"))
+        final PlanPermissions planPermission = new PlanPermissions(new PlanIdentifier("REV", "ONE"))
             .permissions(new Permissions()
-                    .userPermissions("gkumar", PermissionType.EDIT, PermissionType.VIEW, PermissionType.ADMIN, PermissionType.CLONE, PermissionType.BUILD)
-                    .loggedInUserPermissions(PermissionType.VIEW)
-                    .anonymousUserPermissionView());
+                    .userPermissions("rig", PermissionType.EDIT, PermissionType.VIEW, PermissionType.ADMIN, PermissionType.CLONE, PermissionType.BUILD));
         return planPermission;
     }
     
     public static void main(String... argv) {
         //By default credentials are read from the '.credentials' file.
-        BambooServer bambooServer = new BambooServer("http://localhost:8085");
+        BambooServer bambooServer = new BambooServer("http://18.220.143.53:8085/");
         final PlanSpec planSpec = new PlanSpec();
         
         final Plan plan = planSpec.plan();
